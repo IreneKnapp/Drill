6fbb7a4adf9460f0dc7e2ed8ae95e75c4ac647f3
Irene Knapp <irene.knapp@icloud.com>
2012-08-20 20:38:16 -0400

    Initial Commit

---
+++ Med/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Med
+//
+//  Created by Irene Knapp on 8/20/12.
+//  Copyright (c) 2012 Irene Knapp. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ Med/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Med
+//
+//  Created by Irene Knapp on 8/20/12.
+//  Copyright (c) 2012 Irene Knapp. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
